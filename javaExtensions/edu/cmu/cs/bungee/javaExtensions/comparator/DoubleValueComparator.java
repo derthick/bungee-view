e52fa922fb82b737afe220ec093d129a9b66b798
derthick <markderthick@gmail.com>
2016-05-04 16:37:58 -0400

    Move latest version from GitLab.

--- javaExtensions/edu/cmu/cs/bungee/javaExtensions/comparator/DoubleValueComparator.java
+++ javaExtensions/edu/cmu/cs/bungee/javaExtensions/comparator/DoubleValueComparator.java
@@ -2,21 +2,23 @@ package edu.cmu.cs.bungee.javaExtensions.comparator;
 
 import java.util.Comparator;
 
-import edu.cmu.cs.bungee.javaExtensions.Util;
+import edu.cmu.cs.bungee.javaExtensions.UtilMath;
 
 /**
  * Sorts from highest to lowest
  *
  */
-public abstract class DoubleValueComparator<V> implements Comparator<V>{
+public abstract class DoubleValueComparator<V> implements Comparator<V> {
 
-	public int compare(V data1, V data2) {
-		return Util.sgn(value(data2) - value(data1));
+	@Override
+	public int compare(final V data1, final V data2) {
+		return UtilMath.sgn(value(data2) - value(data1));
 	}
 
-	public boolean equals(V data1, V data2) {
-		return value(data1) == value(data2);
-	}
+	// TODO Remove unused code found by UCDetector
+	// public boolean equals(final V data1, final V data2) {
+	// return value(data1) == value(data2);
+	// }
 
 	public abstract double value(V data);
 
--- javaExtensions/edu/cmu/cs/bungee/javaExtensions/comparator/IntValueComparator.java
+++ javaExtensions/edu/cmu/cs/bungee/javaExtensions/comparator/IntValueComparator.java
@@ -1,17 +1,23 @@
 package edu.cmu.cs.bungee.javaExtensions.comparator;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public abstract class IntValueComparator<V> implements Comparator<V> {
+/**
+ * Sorts from highest to lowest.
+ */
+public abstract class IntValueComparator<V> implements Comparator<V>, Serializable {
 
-	public int compare(V data1, V data2) {
-		return value(data2) - value(data1);
+	@Override
+	public int compare(final V v1, final V v2) {
+		return value(v2) - value(v1);
 	}
 
-	public boolean equals(V data1, V data2) {
-		return value(data1) == value(data2);
-	}
+	// TODO Remove unused code found by UCDetector
+	// public boolean equals(V data1, V data2) {
+	// return value(data1) == value(data2);
+	// }
 
-	public abstract int value(V data);
+	public abstract int value(V v);
 
 }
